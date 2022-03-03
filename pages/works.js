892c50cf3b16f2092dfa7dcbdc5b7a3c58210d96
Jason Chan <jasonchanhku@gmail.com>
2022-03-03 22:32:35 +0800

    mma thumbnail retry

--- pages/works.js
+++ pages/works.js
@@ -3,7 +3,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import thumbMma from '../public/images/works/mma.png'
 import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
 import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
 import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
@@ -22,7 +22,7 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="mma" title="Inkdrop" thumbnail={thumbInkdrop}>
+          <WorkGridItem id="mma" title="UFC MMA Predictor" thumbnail={thumbMma}>
             A Markdown note-taking app with 100+ plugins, cross-platform and
             encrypted data sync support
           </WorkGridItem>
