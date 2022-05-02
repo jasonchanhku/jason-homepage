f0351f939069a5213495cf9cf732c3c1c65422b2
Jason Chan <jasonchanhku@gmail.com>
2022-05-02 23:28:06 +0800

    Update hkuai.js

--- pages/competitions/hkuai.js
+++ pages/competitions/hkuai.js
@@ -11,23 +11,26 @@ const Work = () => (
        HKU AI Robotics Vision Competition <Badge>2021</Badge> <Badge>Winner</Badge>
       </Title>
       <P>
-        HKU SAAS Data Science Lab of the Department of Statistics & Actuarial Science, The University of Hong Kong organized this
-        competition is to promote development of artificial intelligence (AI) robotics vision and automation technologies at the school level
-        and the industry level. The competition encourages students and companies to develop innovative AI robotics solutions with AI,
-        data science and statistical tools for solving current hot topics/problems in robotics vision and automation. The competition
-        also serves as a platform for local secondary schools, institutions, and industries to share knowledge, innovation and
-        experience on the application of AI robotics technologies for solving business problems, enhancing businesses’
-        competitiveness, and creating business insights for industries in social science, smart city, healthcare, education,
-        and Internet of Things (IoT).
+                HKU SAAS Data Science Lab organized this competition to promote development of artificial intelligence (AI)
+                robotics vision and automation technologies at the school level and the industry level. The competition encourages students and companies to develop innovative AI robotics solutions with AI,
+                data science and statistical tools for solving current hot topics/problems in robotics vision and automation. The competition
+                also serves as a platform for local secondary schools, institutions, and industries to share knowledge, innovation and
+                experience on the application of AI robotics technologies for solving business problems, enhancing businesses’
+                competitiveness, and creating business insights for industries in social science, smart city, healthcare, education,
+                and Internet of Things (IoT).
       </P>
       <P>
-        I was the team leader and lead coder of our group which developed 
+                I was the team leader and lead coder of our group which developed the solution Artificial Intelligence Video Interview Analyzer (AVIA). AVIA
+                is an AI web application that aims to help students practice for online interviews and assessments as part of the digital transformation
+                caused by COVID-19. The web application uses face detection, facial expression prediction, eye gaze tracking, and sentiment analysis to
+                compute the students' scores when using AVIA. The detailed breakdown of the scores are available in a elegantly summarized dashboard where
+                students can pinpoint their weaknesses during the interview assessment.
       </P>
       <List ml={4} my={4}>
         <ListItem>
-          <Meta>Website</Meta>
-          <Link href="https://www.inkdrop.app/">
-            https://www.inkdrop.app/ <ExternalLinkIcon mx="2px" />
+          <Meta>Solution</Meta>
+          <Link href="https://github.com/jasonchanhku/jasonchanjinan/raw/master/public/hku_article.pdf">
+           Artificial Intelligence Video Interview Analyzer (AVIA) <ExternalLinkIcon mx="2px" />
           </Link>
         </ListItem>
         <ListItem>
